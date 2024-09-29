26a95fd57f77a7bfdf7ac956b753d2258be2e8e3
[Z-Oleksandr] <[zizulforever@gmail.com]>
2024-09-29 19:16:05 +0200

    Draft of backend

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,8 +1,87 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, net::SocketAddr, time::Duration};
 use sysinfo::{NetworkData, Networks, Process, System, Components, Disks};
-use clearscreen;
+use tokio::{net::{TcpListener, TcpStream}, time};
+use tokio_tungstenite::{accept_async, tungstenite::protocol::Message};
+use serde::{Serialize};
+use futures::{StreamExt, SinkExt};
 
-fn main() {
+#[derive(Serialize)]
+struct SystemStats {
+    cpu_usage: f32,
+    ram_total: u64,
+    ram_used: u64,
+    disk_total: u64,
+    disk_used: u64,
+    network_received: u64,
+    network_transmitted: u64,
+}
+
+async fn handle_connection(raw_stream: TcpStream, addr: SocketAddr) {
+    println!("New Socket connection: {}", addr);
+
+    let ws_stream = accept_async(raw_stream).await.expect("Failed to accept");
+
+    // Split ws stream into sender and receiver
+    let (mut write, _) = ws_stream.split();
+
+    let mut sys = System::new_all();
+
+    loop {
+        sys.refresh_all();
+
+        let cpu_usage = sys.global_cpu_usage();
+        let ram_total = sys.total_memory();
+        let ram_used = sys.used_memory();
+
+        // For now just one disk
+        let disks = Disks::new_with_refreshed_list();
+        let disk = disks.get(0).expect("No disk found");
+        let disk_total = disk.total_space();
+        let disk_available = disk.available_space();
+        let disk_used = disk_total - disk_available;
+
+        let networks = Networks::new_with_refreshed_list();
+        let mut net_received = 0;
+        let mut net_transmitted = 0;
+        for (_iface, data) in &networks {
+            net_received += data.total_received();
+            net_transmitted += data.total_transmitted();
+        }
+
+        let stats = SystemStats {
+            cpu_usage,
+            ram_total,
+            ram_used,
+            disk_total,
+            disk_used,
+            network_received: net_received,
+            network_transmitted: net_transmitted,
+        };
+
+        // Serialize stats to JSON
+        let stats_json = serde_json::to_string(&stats).unwrap();
+
+        // Send stats over WebSocket
+        if let Err(e) = write.send(Message::Text(stats_json)).await {
+            eprintln!("Error sending stats: {}", e);
+            break;
+        }
+
+        time::sleep(Duration::from_secs(1)).await;
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "127.0.0.1:8999";
+    let listener = TcpListener::bind(&addr).await.expect("Failed to build");
+    println!("WebSocket server listening on {}", addr);
+
+    while let Ok((stream, addr)) = listener.accept().await {
+        tokio::spawn(handle_connection(stream, addr));
+    }
+
+    // Testing values
     let mut sys = System::new_all();
 
     sys.refresh_all();
@@ -61,10 +140,12 @@ fn main() {
     //     std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
     //     clearscreen::clear().unwrap();
     // }
+    println!("CPUS:");
+    sys.refresh_cpu_all();
+    println!("{:#?}", sys.cpus());
     println!("CPU use rate: ");
     sys.refresh_cpu_all();
     for cpu in sys.cpus() {
         println!("{}%", cpu.cpu_usage());
     }
-    std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
 }
