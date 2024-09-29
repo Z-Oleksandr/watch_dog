d05c51d06db089f2afeb8ea7e3e958246a809627
[Z-Oleksandr] <[zizulforever@gmail.com]>
2024-09-29 12:45:34 +0200

    Test engine

--- engine/src/main.rs
+++ engine/src/main.rs
@@ -1,11 +1,54 @@
-use sysinfo::{NetworkExt, NetworksExt, ProcessExt, System, SystemExt};
+use std::collections::HashMap;
+use sysinfo::{NetworkData, Networks, Process, System, Components, Disks};
 
 fn main() {
-    let mut sys = System.new_all();
+    let mut sys = System::new_all();
 
     sys.refresh_all();
 
-    let cpu_rate = sys.global_processor_info().cpu_usage();
+    let total_memory = sys.total_memory();
+    let used_memory = sys.used_memory();
 
-    println!("CPU rate: {}", cpu_rate);
+    let mut sys_info = HashMap::from([
+        ("name", System::name()),
+        ("kernel", System::kernel_version()),
+        ("os", System::os_version()),
+        ("host", System::host_name()),
+        ("nb_cpu", Some(sys.cpus().len().to_string())),
+    ]);
+
+    // Display processes IP:
+    for (pid, process) in sys.processes() {
+        println!(
+            "[{pid}] {:?} {:?} {:?}", 
+            process.name(), 
+            process.disk_usage(), 
+            process.cpu_usage()
+        )
+    }
+
+    // Display all disk info:
+    println!("=> disks:");
+    let disks = Disks::new_with_refreshed_list();
+    for disk in &disks {
+        println!("{disk:?}");
+    }
+
+    // Network interface name, total data received and transmitted:
+    println!("=> networks:");
+    let networks = Networks::new_with_refreshed_list();
+    for (interface_name, data) in &networks {
+        println!(
+            "{interface_name}: {} B (down) / {} B (up)",
+            data.total_received(),
+            data.total_transmitted(),
+        );
+    }
+
+    // Components temp:
+    let components = Components::new_with_refreshed_list();
+    println!("=> components:");
+    for component in &components {
+        println!("{component:?}");
+    }
 }
